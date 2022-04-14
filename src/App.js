26a156701b68df6130c07e47684add6588293b96
Venkatesh0625 <venkatesan.0625@gmail.com>
2022-04-14 16:23:28 +0530

    Modify secp256k1 version

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import { useEffect } from "react"
 import * as lib from "./lib"
- 
+
 function App() {
 
   useEffect(() => {
-  (async () => {
+    (async () => {
       let message = {
-        chain: 'cosmos', 
-        sender: 'xyz', 
+        chain: 'cosmos',
+        sender: 'xyz',
         type: 'cyd/auth',
         item_hash: 'fjnjkfbjkbg'
       }
 
       let account = await lib.getAccount("casual subway profit cactus impact nature blossom table drift march congress cruel", "m/44'/118'/0'/0/0", "cosmos")
+      console.log({ account })
       let si = await lib.sign(account, message)
       window.si = si
       window.account = account
-      console.log({si})
+      console.log({ si })
     })()
   })
   window.lib = lib
--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,7 @@
 import { sortObject } from './utils'
 const { cosmosjs } = window
 const secp256k1 = require('secp256k1')
-
+const cryptojs = require('crypto-js')
 var Buffer = require('buffer').Buffer
 const CHAIN_ID = "signed-message-v1"
 
@@ -39,10 +39,11 @@ export async function sign(account, message) {
     let signed = cosmos.sign(cosmos.newStdMsg(signable), Buffer.from(account.private_key, 'hex'))
     message.signature = JSON.stringify(signed?.tx?.signatures?.[0])
 
+    let publicKey = Buffer.from(account.public_key, 'hex')
+  
     let signature = signed?.tx?.signatures?.[0]?.signature
-    console.log({signature})
-    console.log(message.signature.signature)
-    verifyMessage(account.public_key, signature,cosmos.newStdMsg(signable))
+    
+    verifyMessage(publicKey, signature,cosmos.newStdMsg(signable))
     return { message, signed }
 }
 
@@ -55,7 +56,7 @@ export const getAccount = async (mnemonic, path, prefix) => {
 
     return {
         'private_key': private_key.toString('hex'),
-        'public_key': secp256k1.publicKeyCreate(private_key).toString('hex'),
+        'public_key': Buffer.from(secp256k1.publicKeyCreate(private_key)).toString('hex'),
         'mnemonic': mnemonic,
         'address': cosmos.getAddress(mnemonic),
         'prefix': prefix,
@@ -64,18 +65,28 @@ export const getAccount = async (mnemonic, path, prefix) => {
 }
 
 export const verifyMessage = async (publicKey, signature, msg) => {
-    let signMessage = new Object;
-    signMessage = msg.json;
 
-    const json = JSON.stringify(sortObject(signMessage))
+    let publicKey_Array = new Uint8Array(Buffer.from(publicKey, 'hex'))
+    
+    let signature_Array = new Uint8Array(Buffer.from(signature, 'base64'))
+
+
+    let signMessage = sortObject(msg.json)
+
+    const json = JSON.stringify(signMessage)
         .replace(/&/g, '\\u0026')
         .replace(/</g, '\\u003c')
         .replace(/>/g, '\\u003e')
 
-    const hash = crypto.createHash('sha256').update(json).digest('hex');
-    const buf = Uint8Array.from(Buffer.from(hash, 'hex'))
-    var bool_ = secp256k1.verify(publicKey, signature, buf);
-    console.log({bool_})
+    const hash = cryptojs.SHA256(json)
+    const message_Array = new Uint8Array(Buffer.from(hash.toString(cryptojs.enc.Hex), 'hex'))
+
+    var bool__ = secp256k1.ecdsaVerify(signature_Array, message_Array, publicKey_Array)
+
+    // For secp256k1 version 3.6.2
+    // var bool_ = secp256k1.verify(buf, signature, publicKey); 
+
+    console.log({bool__})
 }
 
 export const path = "m/44'/118'/0'/0/0"
